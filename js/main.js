4b5f0e0ad38ca3d80614638a159fe5ce8cae3006
Roy <Roy@getacademy.no>
2021-04-25 02:18:04 +0200

    NewStuff

--- js/main.js
+++ js/main.js
@@ -41,13 +41,9 @@ function clickEvolutions()
     page="Evolutions";
     updateNow();
 }
-function clickDiscord()
-{
-    
-}
 function clickContact()
 {
-    page="Contact";
+    //page="Contact";
     updateNow();
 }
 //View
@@ -58,6 +54,7 @@ function updateNow()
         resetColors();
         document.getElementById("home").style.color="#07f5e2";
         document.getElementById("main").innerHTML=`
+        
         <div id="left" onClick="clickEvolutions()">
             <img id="topLeftPic" src="images/ZhenisEvolutions.png">
             <p>Zhenis Evolutions</p>
@@ -133,6 +130,7 @@ function updateNow()
             <img id="botPic" src="images/ZhenisEvolutions3.png">
             <p>Info comming soon to a place near you!!</p>
         </div>
+        
         `;
     }
     else if(page=="Contact")
@@ -162,110 +160,108 @@ function updateNow()
             <div style="text-align: center; margin-top: 0.625rem;" id="paypal-button-container"></div>
         </div>
         <script src="https://www.paypal.com/sdk/js?client-id=AUx3Vo2dB9ZrgvZUZnzGYUpaZF38GST4UPJOudcWYGxlyqyKmG2RPxWmFyjLvijURhefIDeCbolAG05o&currency=USD" data-sdk-integration-source="button-factory"></script>
-        <script>
-        function initPayPalButton() {
-            var description = document.querySelector('#smart-button-container #description');
-            var amount = document.querySelector('#smart-button-container #amount');
-            var descriptionError = document.querySelector('#smart-button-container #descriptionError');
-            var priceError = document.querySelector('#smart-button-container #priceLabelError');
-            var invoiceid = document.querySelector('#smart-button-container #invoiceid');
-            var invoiceidError = document.querySelector('#smart-button-container #invoiceidError');
-            var invoiceidDiv = document.querySelector('#smart-button-container #invoiceidDiv');
 
-            var elArr = [description, amount];
-
-            if (invoiceidDiv.firstChild.innerHTML.length > 1) {
-            invoiceidDiv.style.display = "block";
-            }
-
-            var purchase_units = [];
-            purchase_units[0] = {};
-            purchase_units[0].amount = {};
-
-            function validate(event) {
-            return event.value.length > 0;
-            }
-
-            paypal.Buttons({
-            style: {
-                color: 'gold',
-                shape: 'rect',
-                label: 'paypal',
-                layout: 'vertical',
-                
-            },
-
-            onInit: function (data, actions) {
-                actions.disable();
-
-                if(invoiceidDiv.style.display === "block") {
-                elArr.push(invoiceid);
-                }
-
-                elArr.forEach(function (item) {
-                item.addEventListener('keyup', function (event) {
-                    var result = elArr.every(validate);
-                    if (result) {
-                    actions.enable();
-                    } else {
-                    actions.disable();
-                    }
-                });
-                });
-            },
-
-            onClick: function () {
-                if (description.value.length < 1) {
-                descriptionError.style.visibility = "visible";
-                } else {
-                descriptionError.style.visibility = "hidden";
-                }
-
-                if (amount.value.length < 1) {
-                priceError.style.visibility = "visible";
-                } else {
-                priceError.style.visibility = "hidden";
-                }
-
-                if (invoiceid.value.length < 1 && invoiceidDiv.style.display === "block") {
-                invoiceidError.style.visibility = "visible";
-                } else {
-                invoiceidError.style.visibility = "hidden";
-                }
-
-                purchase_units[0].description = description.value;
-                purchase_units[0].amount.value = amount.value;
-
-                if(invoiceid.value !== '') {
-                purchase_units[0].invoice_id = invoiceid.value;
-                }
-            },
-
-            createOrder: function (data, actions) {
-                return actions.order.create({
-                purchase_units: purchase_units,
-                });
-            },
-
-            onApprove: function (data, actions) {
-                return actions.order.capture().then(function (details) {
-                alert('Transaction completed by ' + details.payer.name.given_name + '!');
-                });
-            },
-
-            onError: function (err) {
-                console.log(err);
-            }
-            }).render('#paypal-button-container');
-        }
-        initPayPalButton();
-        </script>
         `;
     }
 }
 
 
+function initPayPalButton() {
+    var description = document.querySelector('#smart-button-container #description');
+    var amount = document.querySelector('#smart-button-container #amount');
+    var descriptionError = document.querySelector('#smart-button-container #descriptionError');
+    var priceError = document.querySelector('#smart-button-container #priceLabelError');
+    var invoiceid = document.querySelector('#smart-button-container #invoiceid');
+    var invoiceidError = document.querySelector('#smart-button-container #invoiceidError');
+    var invoiceidDiv = document.querySelector('#smart-button-container #invoiceidDiv');
 
+    var elArr = [description, amount];
+
+    if (invoiceidDiv.firstChild.innerHTML.length > 1) {
+    invoiceidDiv.style.display = "block";
+    }
+
+    var purchase_units = [];
+    purchase_units[0] = {};
+    purchase_units[0].amount = {};
+
+    function validate(event) {
+    return event.value.length > 0;
+    }
+
+    paypal.Buttons({
+    style: {
+        color: 'gold',
+        shape: 'rect',
+        label: 'paypal',
+        layout: 'vertical',
+        
+    },
+
+    onInit: function (data, actions) {
+        actions.disable();
+
+        if(invoiceidDiv.style.display === "block") {
+        elArr.push(invoiceid);
+        }
+
+        elArr.forEach(function (item) {
+        item.addEventListener('keyup', function (event) {
+            var result = elArr.every(validate);
+            if (result) {
+            actions.enable();
+            } else {
+            actions.disable();
+            }
+        });
+        });
+    },
+
+    onClick: function () {
+        if (description.value.length < 1) {
+        descriptionError.style.visibility = "visible";
+        } else {
+        descriptionError.style.visibility = "hidden";
+        }
+
+        if (amount.value.length < 1) {
+        priceError.style.visibility = "visible";
+        } else {
+        priceError.style.visibility = "hidden";
+        }
+
+        if (invoiceid.value.length < 1 && invoiceidDiv.style.display === "block") {
+        invoiceidError.style.visibility = "visible";
+        } else {
+        invoiceidError.style.visibility = "hidden";
+        }
+
+        purchase_units[0].description = description.value;
+        purchase_units[0].amount.value = amount.value;
+
+        if(invoiceid.value !== '') {
+        purchase_units[0].invoice_id = invoiceid.value;
+        }
+    },
+
+    createOrder: function (data, actions) {
+        return actions.order.create({
+        purchase_units: purchase_units,
+        });
+    },
+
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+        alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+    },
+
+    onError: function (err) {
+        console.log(err);
+    }
+    }).render('#paypal-button-container');
+}
+initPayPalButton();
 
 
 //Dette er til spillet Game1 som er tick tack toe.
